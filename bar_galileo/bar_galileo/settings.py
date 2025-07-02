926b00232a805bd9ae266408c15fa63c4b883e65
Stevan <stevanpre@gmail.com>
2025-07-02 11:49:26 -0500

    diseño

--- bar_galileo/bar_galileo/settings.py
+++ bar_galileo/bar_galileo/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,7 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# URL para acceder a los archivos estáticos
+STATIC_URL = '/static/'
+
+# Ruta a la carpeta donde están tus archivos estáticos (como css, js, img)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
